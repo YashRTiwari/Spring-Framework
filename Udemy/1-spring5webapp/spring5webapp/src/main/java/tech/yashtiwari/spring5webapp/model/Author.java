49fa1538e093b3dcfb02f21e054b1434a0bfe9d1
Yash Rajendra Tiwari <work.yrt@gmail.com>
2020-05-25 00:43:03 -0500

    Added JPA Entity

--- Udemy/1-spring5webapp/spring5webapp/src/main/java/tech/yashtiwari/spring5webapp/model/Author.java
+++ Udemy/1-spring5webapp/spring5webapp/src/main/java/tech/yashtiwari/spring5webapp/model/Author.java
@@ -1,14 +1,31 @@
 package tech.yashtiwari.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     private String fName;
     private String lName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public Author(String fName, String lName, Set<Book> books) {
@@ -39,5 +56,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id == author.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
     }
 }
--- Udemy/1-spring5webapp/spring5webapp/src/main/java/tech/yashtiwari/spring5webapp/model/Book.java
+++ Udemy/1-spring5webapp/spring5webapp/src/main/java/tech/yashtiwari/spring5webapp/model/Book.java
@@ -1,14 +1,33 @@
 package tech.yashtiwari.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public Book(String title, String isbn, Set<Author> authors) {
@@ -39,5 +58,30 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
     }
 }
